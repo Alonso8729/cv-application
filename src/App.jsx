ebc36c3a4f718cf6fb4569b3726e2aeb7317884d
Alonso8729 <alon8729@gmail.com>
2023-11-16 14:25:02 +0200

    Added education events Functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import './styles/App.css'
 import Personal from './components/Personal-Details/Personal'
 import Resume from './components/Resume'
+import Education from './components/Education-Details/Education'
+import uniqid from 'uniqid';
+
 
 function App() {
   const [personalInfo, setPersonalInfo] = useState({ fullName: '', email: '', phoneNumber: '', address: '' })
+  const [educationInfo, setEducationInfo] = useState({ school: '', degree: '', startDate: '', endDate: '', location: '', id: uniqid() })
+  const [isEducationOpen, setIsEducationOpen] = useState(false);
+  const [educationList, setEducationList] = ([])
 
   const handlePersonalInfoChange = (e) => {
     const { key } = e.target.dataset
@@ -15,6 +21,40 @@ function App() {
       }
     })
   }
+
+  const handleEducationInfoChange = (e) => {
+    const { key } = e.target.dataset
+    setEducationInfo((prevInfo) => {
+      return {
+        ...prevInfo,
+        [key]: e.target.value
+      }
+    })
+  }
+
+  const handleEducationDelete = (index) => {
+    const updatedList = { ...educationList}
+    updatedList.splice(index,1);
+    setEducationList(updatedList)
+  }
+
+  const handleToggle = (e) => {
+    const { key } = e.target.dataset
+    if (key === 'education') {
+      setIsEducationOpen(!isEducationOpen)
+    }
+  }
+
+  const handleAddEducation = (e) => {
+    const { key } = e.target.dateset
+    setEducationList((prevList) => {
+      return {
+        ...prevList,
+        [key]: e.target.value
+      }
+    })
+  }
+
   return (
     <div className='app'>
       <div className='edit-container'>
@@ -25,13 +65,17 @@ function App() {
           phoneNumber={personalInfo.phoneNumber}
           address={personalInfo.address}
         />
+        <Education
+          educationList={educationList}
+          educationInfo={educationInfo}
+          onClick={handleToggle}
+          isOpen={isEducationOpen}
+          onChange={handleEducationInfoChange}
+          onDelete={handleEducationDelete} />
       </div>
       <div className="resume-container">
         <Resume
-          email={personalInfo.email}
-          fullName={personalInfo.fullName}
-          phoneNumber={personalInfo.phoneNumber}
-          address={personalInfo.address}
+          personalInfo={personalInfo}
         />
       </div>
     </div>
