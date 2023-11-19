ec1bbd36f2ce6934065db108c5d2fcade93af525
Alonso8729 <alon8729@gmail.com>
2023-11-19 16:05:38 +0200

    Added handleCancel and handleAddEducation

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,19 @@ import uniqid from 'uniqid';
 function App() {
   const [personalInfo, setPersonalInfo] = useState(ExampleData.personalInfo)
   const [educationInfo, setEducationInfo] = useState({
-    companyName: "",
-    positionTitle: "",
+    degree: "",
+    school: "",
     location: "",
-    description: "",
     startDate: "",
     endDate: "",
-    id: "",
+    id: uniqid(),
   })
   const [isEducationOpen, setIsEducationOpen] = useState(false);
-  const [educationList, setEducationList] = useState(ExampleData.education)
-  
+  const [educationList, setEducationList] = useState(ExampleData.education);
+  const [educationForm, setEducationForm] = useState(false)
+
   const handlePersonalInfoChange = (e) => {
+    e.preventDefault()
     const { key } = e.target.dataset
     setPersonalInfo((prevInfo) => {
       return {
@@ -32,6 +33,7 @@ function App() {
   }
 
   const handleEducationInfoChange = (e) => {
+    e.preventDefault()
     const { key } = e.target.dataset
     setEducationInfo((prevInfo) => {
       return {
@@ -45,23 +47,49 @@ function App() {
     const updatedList = { ...educationList }
     updatedList.splice(index, 1);
     setEducationList(updatedList)
+
   }
 
   const handleToggle = (e) => {
+    e.preventDefault()
     const { key } = e.target.dataset
     if (key === 'education') {
       setIsEducationOpen(!isEducationOpen)
     }
   }
 
-  const handleAddEducation = (e) => {
-    const { key } = e.target.dateset
-    setEducationList((prevList) => {
-      return {
-        ...prevList,
-        [key]: e.target.value
-      }
+  const handleAddEducation = () => {
+    setEducationList((prevList) => [...prevList, educationInfo])
+    setEducationInfo({
+      degree: "",
+      school: "",
+      location: "",
+      startDate: "",
+      endDate: "",
+      id: uniqid(),
     })
+    setEducationForm(!educationForm);
+  }
+
+  const handleCancel = (section) => {
+    if (section === 'education') {
+      setEducationInfo({
+        degree: "",
+        school: "",
+        location: "",
+        startDate: "",
+        endDate: "",
+        id: uniqid(),
+      });
+      setEducationForm(!educationForm)
+    }
+  }
+
+
+
+
+  const createForm = (sectionName) => {
+
   }
 
   return (
@@ -76,11 +104,18 @@ function App() {
         />
         <Education
           educationList={educationList}
+          educationInfo={educationInfo}
           onClick={handleToggle}
           isOpen={isEducationOpen}
           onChange={handleEducationInfoChange}
           onDelete={handleEducationDelete}
-          onToggle={handleToggle} />
+          onToggle={handleToggle}
+          onSave={handleAddEducation}
+          onCancel={()=>handleCancel('education')}
+          isForm={educationForm}
+          onButton={() => setEducationForm(!educationForm)}
+        />
+
       </div>
       <div className="resume-container">
         <Resume
