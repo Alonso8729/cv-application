72ff65178817b69c090601fae20ec697559086df
Alonso8729 <alon8729@gmail.com>
2023-11-19 09:31:57 +0200

    Added Fragment

--- src/components/Education-Details/Education.jsx
+++ src/components/Education-Details/Education.jsx
@@ -1,6 +1,6 @@
 import '../../styles/Education.css'
 
-export default function Education({ isOpen, onToggle, educationInfo, educationList,handleDelete }) {
+export default function Education({ isOpen, onToggle, educationList, handleDelete }) {
     return (
         <div className="education-details">
             <div className="education-header">
@@ -9,32 +9,35 @@ export default function Education({ isOpen, onToggle, educationInfo, educationLi
                     <h2 className="education-title">Education Details</h2>
                 </div>
                 <div className='right-header'>
-                    <i data-key="education" className={`fa-solid fa-chevron-down ${isOpen ? 'open' : ''}`} onClick={onClick}></i>
+                    <i data-key="education" className={`fa-solid fa-chevron-down ${isOpen ? 'open' : ''}`} onClick={onToggle}></i>
                 </div>
             </div>
             {/** Display education content if open*/}
-            {isOpen &
-                <div className='education-content'>
-                    {educationList.length > 0 ?
-                        <div className="education-list">
-                            <ul>
-                                {educationList.map((item, index) =>
-                                    <div className='education-item'>
-                                        <li onClick={onToggle} key={index}>{item.school}
-                                        </li>
-                                        <i onClick={()=>handleDelete(index)} className="fa-solid fa-trash-can"></i>
-                                    </div>
-                                )
-                                }
-                            </ul>
-                        </div>
-
-                        :
-                        ''
-                    }
-
-                </div>
-
+            {isOpen &&
+                <>
+                    <div className="education-content">
+                        {educationList.length > 0 ?
+                            <div className='education-list'>
+                                <ul>
+                                    {educationList.map((item) =>
+                                        <div className='education-item'>
+                                            <li onClick={onToggle} key={item.key}>
+                                                {item.school}
+                                            </li>
+                                            <i onClick={() => handleDelete(index)} className="fa-solid fa-trash-can"></i>
+                                        </div>
+                                    )
+                                    }
+                                </ul>
+                            </div>
+                            :
+                            <p>156651561651</p>
+                        }
+                    </div>
+                    <div className="add-button-ctn">
+                        <button className='add-ed-button'><i class="fa-solid fa-plus"></i>Education</button>
+                    </div>
+                </>
             }
         </div >
     )
