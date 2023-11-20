f26481b752b58bee04b42d9da4f7e450b0c0cc9a
Alonso8729 <alon8729@gmail.com>
2023-11-20 11:10:26 +0200

    Implemented component

---
+++ src/components/Personal/Personal.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react"
+import InputDiv from "../InputDiv"
+import '../../styles/Personal.css'
+
+export default function Personal({ email, fullName, phoneNumber, address, onChange }) {
+
+    return (
+        <form className="personal-details">
+            <h2>Personal Details</h2>
+            <InputDiv
+                onChange={onChange}
+                type="text"
+                id="full-name"
+                placeholder="Enter first and last name"
+                value={fullName}
+                label="Full Name"
+                data-key="fullName"
+            />
+            <InputDiv
+                onChange={onChange}
+                type="email"
+                id="email"
+                placeholder="Enter email"
+                value={email}
+                label="Email"
+                data-key="email"
+            />
+            <InputDiv
+                onChange={onChange}
+                type="tel"
+                id="phone-number"
+                placeholder="Enter phone number"
+                value={phoneNumber}
+                label="Phone number"
+                data-key="phoneNumber"
+            />
+            <InputDiv
+                onChange={onChange}
+                type="text"
+                id="address"
+                placeholder="Enter City, Country"
+                value={address}
+                label="Address"
+                data-key="address"
+            />
+        </form>
+    )
+
+
+}
