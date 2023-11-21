1e8e2df44141292cadcfe77b8271392f228ef9de
Alonso8729 <alon8729@gmail.com>
2023-11-21 16:12:57 +0200

    Implemented download feature

--- src/components/ResumePDF.jsx
+++ src/components/ResumePDF.jsx
@@ -1,43 +1,22 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Resume from './Resume';
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
 
-export default function ResumePDF({
-    personalInfo,
+
+
+export default function ResumePDF({ personalInfo,
     educationList,
     experienceList,
-    skillsList
+    skillsList,
 }) {
-
-    const pdfRef = useRef()
-    const handleDownload = () => {
-        const input = pdfRef.current;
-        html2canvas(input).then((canvas) => {
-            const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF('p', 'mm', 'a4', true);
-            const pdfWidth = pdf.internal.pageSize.getWidth();
-            const pdfHeight = pdf.internal.pageSize.getHeight();
-            const imgWidth = canvas.width;
-            const imgHeight = canvas.height;
-            const ratio = Math.min(pdfWidth / imgWidth, pdfHeight, imgHeight);
-            const imgX = (pdfWidth - imgWidth * ratio) / 2;
-            const imgY = 30;
-            pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio)
-            pdf.save('invoice.pdf')
-        })
-
-
-        return (
+    return (
+        <div>
             <Resume
-                className={pdfRef}
                 personalInfo={personalInfo}
                 educationList={educationList}
                 experienceList={experienceList}
                 skillsList={skillsList}
             />
-        );
-    }
+        </div>
+    );
 }
 
-export { handleDownload }
---
+++ src/components/Template/TemplateSection.jsx
@@ -0,0 +1,52 @@
+import '../../styles/TemplateSection.css'
+import React, { useRef } from 'react'
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import ResumePDF from '../ResumePdf';
+
+export default function TemplateSection({
+    onClear,
+    onLoad,
+    personalInfo,
+    educationList,
+    experienceList,
+    skillsList,
+}) {
+
+
+    const handleDownload = () => {
+        const capture = document.querySelector('.resume-container')
+        html2canvas(capture).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF('p', 'mm', 'a4');
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = pdf.internal.pageSize.getHeight();
+            pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+            pdf.save('resume.pdf');
+        });
+    };
+
+    return (
+        <div className='template-ctn'>
+            <div className="template-btns">
+                <button onClick={onClear} className='clear-btn' type='button'>
+                    <i className="fa-solid fa-trash"></i>Clear Resume
+                </button>
+                <button onClick={onLoad} className='example-btn' type="button">
+                    Load Example
+                </button>
+                <div style={{ display: 'none' }}>
+                    <ResumePDF
+                        personalInfo={personalInfo}
+                        educationList={educationList}
+                        experienceList={experienceList}
+                        skillsList={skillsList}
+                    />
+                </div>
+                <button onClick={handleDownload} className='download-btn'>
+                    <i className="fa-regular fa-file-pdf"></i>Download
+                </button>
+            </div>
+        </div>
+    );
+}
