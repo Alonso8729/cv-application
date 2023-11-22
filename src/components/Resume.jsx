a275a44b0aa402cd0619bf9b8cd163f999e4de6c
Alonso8729 <alon8729@gmail.com>
2023-11-22 17:09:07 +0200

    Implemented view mode click manipulation

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,11 +5,11 @@ import EducationPreview from "./Education/EducationPreview";
 import ExperiencePreview from "./Experience/ExperiencePreview"
 import SkillsPreview from "./Skills/SkillsPreview";
 
-export default function Resume({ personalInfo, educationList, experienceList, skillsList }) {
+export default function Resume({ personalInfo, educationList, experienceList, skillsList, isViewMode }) {
     const { email, fullName, phoneNumber, address } = personalInfo
     return (
         <div className="resume-ctn">
-            <div className="resume">
+            <div className={`resume ${isViewMode ? '' : 'hide'}`}>
                 <PersonalPreview
                     email={email}
                     fullName={fullName}
